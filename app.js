2d5e538c54d73245cb5164309a6d478a50d400ff
Walter Marinho <waltersmarinho@edu.unifil.br>
2022-11-08 13:50:51 -0300

    Usando ejs para mostrar views

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 var app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
     res.send("<html><body>Portal de Notícias</body></html>")
 });
+
 app.get('/tecnologia', function (req, res) {
-    res.send("<html><body>Notícias de tecnologia</body></html>")
+    res.render("secao/tecnologia");
 });
 
 app.get('/moda', (req, res) =>
